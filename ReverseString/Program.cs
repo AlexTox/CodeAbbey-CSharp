954c63aba19150bb2a2a2aa5e98635dc6c080755
Aleksej Kostrykin <aleksejkostrykin@icloud.com>
2020-07-30 20:11:55 +0300

    Reverse String
    Problem #30

---
+++ ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(Console.ReadLine()));
+        }
+
+
+        public static string Reverse(string stringForReverse)
+        {
+            char[] charArray = stringForReverse.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+
+    }
+}
