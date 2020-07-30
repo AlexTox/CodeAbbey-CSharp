90e42c3e36df23c2183efc1a43d9740f7213d18d
Aleksej Kostrykin <aleksejkostrykin@icloud.com>
2020-07-30 20:51:11 +0300

    Josephus Problem
    Problem #32

---
+++ JosephusProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JosephusProblem
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine(Josephus(input[0], input[1]));
+        }
+
+        static int Josephus(int n, int k)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return (Josephus(n - 1, k) + k - 1) % n + 1;
+        }
+
+
+        
+    }
+}
