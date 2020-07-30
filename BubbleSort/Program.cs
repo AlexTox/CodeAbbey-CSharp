a6eae650e2d7506e27eae48a3c00981c401579df
Aleksej Kostrykin <aleksejkostrykin@icloud.com>
2020-07-30 15:51:53 +0300

    Bubble Sort
    Problem #27

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        private static int counterCycles = 0;
+        private static int counterSwap = 0;
+        static void Main(string[] args)
+        {
+            BoggleSort(Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse));
+
+            Console.Write(counterCycles + " " + counterSwap);
+        }
+
+        private static int[] BoggleSort(int[] arrayForSort)
+        {
+            while (true)
+            {
+                counterCycles++;
+
+                int internalCounter = 0;
+
+                for (int i = 0; i < arrayForSort.Length - 1; i++)
+                {
+                    if(arrayForSort[i] > arrayForSort[i + 1])
+                    {
+                        int temp = arrayForSort[i];
+                        arrayForSort[i] = arrayForSort[i + 1];
+                        arrayForSort[i + 1] = temp;
+
+                        counterSwap++;
+                        internalCounter++;
+                    }
+                }
+
+                if (internalCounter == 0)
+                {
+                    break;
+                }
+            }
+
+            return arrayForSort;
+        }
+    }
+}
