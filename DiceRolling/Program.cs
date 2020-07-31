a91a94d2d7547426a264bd204fbbdc65417842b0
Aleksej Kostrykin <aleksejkostrykin@icloud.com>
2020-07-31 14:43:16 +0300

    Dice Rolling and Median Of Three

---
+++ DiceRolling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace DiceRolling
+{
+    class Program
+    {
+        public static int[] Numbers;
+        public static int MinRange = 1;
+        public static int MaxRange = 6;
+        static void Main(string[] args)
+        {
+            Numbers = new int[int.Parse(Console.ReadLine())];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                var newNumberLine = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Numbers[i] = (int) Math.Floor(newNumberLine * (MaxRange)) + MinRange;
+            }
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.Write(Numbers[i] + " ");
+            }
+        }
+    }
+}
---
+++ MedianOfThree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MedianOfThree
+{
+    class Program
+    {
+        public static double[] Medians;
+        static void Main(string[] args)
+        {
+            Medians = new double[int.Parse(Console.ReadLine())];
+
+            for (int i = 0; i < Medians.Length; i++)
+            {
+                var newNumberLine = Array.ConvertAll(Console.ReadLine().Split(" "), double.Parse);
+
+                Medians[i] = GetMedian(newNumberLine);
+            }
+
+            for (int i = 0; i < Medians.Length; i++)
+            {
+                Console.Write(Medians[i] + " ");
+            }
+        }
+
+        public static double GetMedian(double[] sourceNumbers)
+        {    
+            if (sourceNumbers == null || sourceNumbers.Length == 0)
+                throw new System.Exception("Median of empty array not defined.");
+
+            double[] sortedPNumbers = (double[])sourceNumbers.Clone();
+            Array.Sort(sortedPNumbers);
+
+            int size = sortedPNumbers.Length;
+            int mid = size / 2;
+            double median = (size % 2 != 0) ? (double)sortedPNumbers[mid] : ((double)sortedPNumbers[mid] + (double)sortedPNumbers[mid - 1]) / 2;
+
+            return median;
+        }
+    }
+}
